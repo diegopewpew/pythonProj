37afb3b67d0ce99a692b8cff1457f70fe8b7affa
diegopewpew <62654760+diegopewpew@users.noreply.github.com>
2024-02-12 20:47:43 +0800

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
-
-
-# In[2]:
 
 import os
 def display_board(board):     #simple displaying board 
@@ -18,23 +10,16 @@ def display_board(board):     #simple displaying board
     print (' '+board[1]+' | '+board[2]+' | '+board[3])
 
 
-# In[ ]:
-
-
-
-
-
-# In[3]:
 
 
 test_board = ['#','X','O','X','O','X','O','X','O','X'] #test
 display_board(test_board)
 
 
-# In[4]:
 
 
-def player_input():             #take input from user and assign it to tuple 
+
+def player_input():             
     marker = ''
     
     while not (marker == 'X' or marker == 'O'):
@@ -46,27 +31,24 @@ def player_input():             #take input from user and assign it to tuple
         return ('O', 'X')
 
 
-# In[5]:
 
 
-player_input()     #test
+player_input()    
 
 
-# In[6]:
 
 
 def place_marker(board, marker , position):       #allow user to make x and o in desired position
     board[position] = marker
 
 
-# In[7]:
 
 
 place_marker(test_board,'X',5)    #test
 display_board(test_board)
 
 
-# In[8]:
+
 
 
 def win_check(board,mark):             #checks all winning possiblity
@@ -80,13 +62,13 @@ def win_check(board,mark):             #checks all winning possiblity
           (board[7] == mark and board[5] == mark and board[3] == mark) )
 
 
-# In[9]:
+
 
 
 win_check(test_board,'X')
 
 
-# In[10]:
+
 
 
 import random          #randomly return who will play first turn
@@ -98,23 +80,22 @@ def choose_first():
         return 'Player 2'
 
 
-# In[ ]:
 
 
 
 
 
-# In[11]:
+
 
 
 def space_check(board,position):  #returns all position blank 
     return board[position] == ' '
 
 
-# In[12]:
 
 
-def full_board_check(board):   #checks all position if its filled or empty
+
+def full_board_check(board):  
     for i in range(1,10):
         if space_check(board,i):
             return False
@@ -122,10 +103,10 @@ def full_board_check(board):   #checks all position if its filled or empty
         
 
 
-# In[13]:
 
 
-def player_choice(board):        #takes input from user and also checks space on board 
+
+def player_choice(board):        
     position = 0
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board,position):
         position = int(input("enter position from (1-9): "))
@@ -133,44 +114,44 @@ def player_choice(board):        #takes input from user and also checks space on
     return position
 
 
-# In[14]:
 
 
-def replay():     #function for playing again 
+
+def replay():    
      return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
 
 
-# In[ ]:
+
 
 
 print ('Welcome welcome welcome!!')
 while True:
     the_board = [' ']*10
-    player1_marker , player2_marker = player_input() #tuple unpacking
+    player1_marker , player2_marker = player_input()
     
     turn = choose_first()
     print (turn +' will go first')
     
-    play_game = input ('ready to play?Y or N').lower() #start of game and confirmation from user to start a game
+    play_game = input ('ready to play?Y or N').lower() 
     
     if play_game == 'y':
         game_on = True
     else :
         game_on = False
     
-    while game_on:   #game begin 
+    while game_on: 
         
         if turn == 'Player 1':
-            #displaying board
+           
             display_board(the_board)
-            #input from user mark it on board
+          
             position = player_choice(the_board)
             place_marker(the_board,player1_marker,position)
             
             if win_check (the_board,player1_marker):
                 
                 display_board(the_board)
-                print ("Player 1 WINS!!!")       #condition for winning
+                print ("Player 1 WINS!!!")     
                 game_on = False
                 
             else:
@@ -184,7 +165,7 @@ while True:
                     turn = 'Player 2'
                     
         else :
-            #player 2 turn
+          
             display_board(the_board)
             position = player_choice(the_board)
             place_marker(the_board,player2_marker,position)
@@ -205,18 +186,3 @@ while True:
         break
 
 
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
